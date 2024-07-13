5e808ae82eb85f75321f3453d44ad88a3e13e1ea
jeevithaeluamali <jeevithaelumalai9203@gmail.com>
2024-07-13 20:51:07 +0530

    add

--- sentimentanalyzer/sentimentanalyzer/settings.py
+++ sentimentanalyzer/sentimentanalyzer/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7sbof^l@g0ryr3w!*&#^ds-h%w*5wc#605n)9j7g*&yg%7cgh)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'analyzer/static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
